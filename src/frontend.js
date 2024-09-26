93f9a1f046faaaf5d6f5723d3d7831535a7b71e3
RogerDewald <daniel.r.dewald@gmail.com>
2024-09-26 01:53:55 -0500

    dwadwadwxc

--- src/frontend.js
+++ src/frontend.js
@@ -388,7 +388,8 @@ async function calculateAllVerses() {
     // Make the GET request to retrieve records
     const response = await fetch(url, { headers })
     const json = response.json()
-    console.log(response)
-    console.log(json)
+    console.log(json.data)
+    console.log(json.data.records)
+    json.data.records.foreach(e => console.log(e))
 
 }
