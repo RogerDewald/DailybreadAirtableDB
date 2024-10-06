c21adcac7a0eff61deb9d0b2f3159ea364b543e6
RogerDewald <daniel.r.dewald@gmail.com>
2024-10-06 13:19:27 -0500

    smoothing out the bugs

--- src/frontend.js
+++ src/frontend.js
@@ -1,8 +1,15 @@
 createChapterSelect()
+app = ""
+db = ""
 
-const firebaseConfig = await fetch("/api/getFirebaseAPI")
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+async function firebaseInit() {
+    const firebaseConfig = await fetch("/api/getFirebaseAPI")
+    const app = firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
+}
+
+window.addEventListener("load", firebaseInit())
+
 
 const allVersesArray = [[],
 ('Matthew', 28, [[], 25, 23, 17, 25, 48, 34, 29, 34, 38, 42, 30, 50, 58, 36, 39, 28, 27, 35, 30, 34, 46, 46, 39, 51, 46, 75, 66, 20]),
